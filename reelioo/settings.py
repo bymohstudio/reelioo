aaee0aa92551dbf83122d1d632944b796a48bd62
mohit <mohitmishra309@outlook.com>
2025-12-25 17:29:15 +0530

    Fixed UI for alpha

--- reelioo/settings.py
+++ reelioo/settings.py
@@ -83,17 +83,21 @@ WSGI_APPLICATION = 'reelioo.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 if os.getenv("DATABASE_URL"):
-    # Only use Postgres if DATABASE_URL is set
     import dj_database_url
     DATABASES = {
-        'default': dj_database_url.parse(os.getenv("DATABASE_URL"))
+        "default": dj_database_url.parse(
+            os.getenv("DATABASE_URL"),
+            conn_max_age=600,
+        )
     }
 else:
-    # Default = SQLite (safe for Railway)
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+            "NAME": "/data/db.sqlite3",
+            "OPTIONS": {
+                "timeout": 20,
+            }
         }
     }
 # If REDIS_URL exists (e.g. on Railway), use Redis.
